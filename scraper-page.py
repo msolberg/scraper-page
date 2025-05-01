7b91a3862a57c90f8f074494a54db0bda8948778
Michael Solberg <msolberg@redhat.com>
2025-05-01 10:04:01 -0400

    Add some basic exception handling for grabbing the URL

--- scraper-page.py
+++ scraper-page.py
@@ -14,28 +14,32 @@ height.set(1.2)
 siteCode = "02333500"
 
 def get_river_height(siteCode="02333500"):
-    waterservices_url = 'https://waterservices.usgs.gov/nwis/iv/?format=json&sites=%s&siteStatus=all'% siteCode
-    with urllib.request.urlopen(waterservices_url) as response:
-        json_response = response.read()
-
-    stats = json.loads(json_response)
     variable = None
     value = None
+    waterservices_url = 'https://waterservices.usgs.gov/nwis/iv/?format=json&sites=%s&siteStatus=all'% siteCode
+    try:
+        with urllib.request.urlopen(waterservices_url) as response:
+            json_response = response.read()
     
-    for v in stats['value']['timeSeries']:
-        if v['variable']['variableCode'][0]['value'] == "00065":
-            variable = v
-            value = v['values'][0]['value'][0]['value']
-
-    height.set(float(value))
-    print("River height %s"% value)
+        stats = json.loads(json_response)
+        
+        for v in stats['value']['timeSeries']:
+            if v['variable']['variableCode'][0]['value'] == "00065":
+                variable = v
+                value = v['values'][0]['value'][0]['value']
+        
+        height.set(float(value))
+        print("River height %s"% value)
+    
+    except urllib.error.URLError as e:
+        print("Error accessing waterservices.usgs.gov: %s"% e.reason)
     
     return value
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
     start_http_server(8000)
-    # Generate some requests.
+    
     while True:
         get_river_height(siteCode="02333500")
         time.sleep(60)
